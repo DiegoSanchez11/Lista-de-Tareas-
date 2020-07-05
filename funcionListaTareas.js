f598a7631a69408b4496402d5383304384854fa0
Diego Sanchez <diegominepro@gmail.com>
2020-07-05 14:04:58 -0300

    agregando parte del codigo js

--- funcionListaTareas.js
+++ funcionListaTareas.js
@@ -1,1 +1,29 @@
-(function(){}());
+(function(){
+    //variables
+    var lista = document.getElementById("lista");
+    var tareaInput = document.getElementById("tareaInput");
+    var btnNuevaTarea = document.getElementById("btn-agregar");
+
+    // Funciones
+    var agregarTarea = function(){
+        alert("Agregar Tarea")
+    };
+    var comprobarInput = function(){
+        alert("Comprobar Input")
+    };
+    var eliminarTarea = function(){
+        alert("Eliminar Tarea")
+    };
+    // Eventos
+
+    // Agregar Tarea
+    btnNuevaTarea.addEventListener("click", agregarTarea);
+
+    // Comprobar Input
+    tareaInput.addEventListener("click", comprobarInput);
+    
+    // Borrando elementos de la lista
+    for (var i = 0; i <= lista.children.length -1; i++) {
+        lista.children[i].addEventListener("click", eliminarTarea);
+    }
+}());
